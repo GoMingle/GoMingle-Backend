d76387b09c38651afe3c2412b95a45e266ccfe98
Abigail Asante <asantea331@gmail.com>
2024-07-02 17:23:43 +0000

    added userController and userRoute

--- controllers/eventControllers.js
+++ controllers/eventControllers.js
@@ -1,43 +1,54 @@
+import { EventModel } from "../models/eventModel.js";
 
 
+
+
+// Get Event
+export const getEvents = async (req, res, next) => {
+   try {
+       // Get events from database
+       const retrieveEvents = await EventModel.find();
+       res.json(retrieveEvents);
+   } catch (error) {
+       next(error);
+   }
+}
 
 
 // post Event
 export const postEvent = async (req, res, next) => {
-    try {
-        // Add event to database
-        const newEvent = await eventModel.create(req.body);
-        // Return response
-        res.json(newEvent)
-    } catch (error) {
-        next(error)
-    }
+   try {
+       // Add event to database
+       const newEvent = await EventModel.create(req.body);
+       // Return response
+       res.json(newEvent)
+   } catch (error) {
+       next(error)
+   }
 };
 
+
 // update Event
-// 
+//
 export const patchEvent = async (req, res, next) => {
-    try {
-        // update event by id
-        const updateEvent = await eventModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
-        // Return response
-        res.json(updateEvent)
-    } catch (error) {
-        next(error)   
-    } 
-};
+   try {
+       const updateEvents = await EventModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+       res.json(updateEvents);
+       // console.log(updateEvents);
+   } catch (error) {
+       next(error)
+   }
+}
 
 // Delete Event
 export const deleteEvent = async (req, res, next) => {
     try {
         // remove an event
-        const removeEvent = await eventModel.findByIdAndDelete(req.params.body)
+        const removeEvent = await EventModel.findByIdAndDelete(req.params.body)
         // return response
         res.json(removeEvent)
     } catch (error) {
         next(error)
     }
-}
-
-
-    
+ }
+ 
