bd306675be8c1a9bd5683ce52cdcb9c86cf6287e
Dennis Appah <dev.dennisappah@gmail.com>
2024-07-03 17:33:46 +0000

    created remote file upload

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,13 @@
-import {schema, model, Types} from mongoose;
+import {Schema, model, Types} from "mongoose";
 
 const userSchema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    username: {type: true, required: true},
+    username: {type: String, required: true},
     eventId: {type: Types.ObjectId, ref: 'event'},
     favoriteCategory: {type: [Types.ObjectId], ref: 'category'}
 }, {
     timestamps: true,
 })
 
-export const userModel = model('user', userModel);
+export const userModel = model('user', userSchema);
