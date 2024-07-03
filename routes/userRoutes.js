0da1f6efca9a20ccf86e85fa92a986ee526c1d2e
Abigail Asante <asantea331@gmail.com>
2024-07-03 16:13:50 +0000

    adding user signUp and login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import { signupUser, loginUser } from "../controllers/auth_controller.js";
 import { postUser, patchUser, deleteUser } from "../controllers/userControllers.js";
 
-
+// define route
 const userRouter = Router();
+
+// userlogin and signup
+userRouter.post('/user', signupUser);
+userRouter.post('/user', loginUser);
 
 userRouter.post('/user', postUser);
 userRouter.patch('/user', patchUser);
